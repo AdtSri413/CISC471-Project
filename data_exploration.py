3b3216cb929c85379b2f127d8e8eefd2c9fd1ab7
AdtSri413 <aditi.srinivasan@gmail.com>
2022-10-12 18:53:33 -0400

    Fixed headers

--- data_exploration.py
+++ data_exploration.py
@@ -9,9 +9,11 @@ import math
 
 
 ''' pretty printer for dicts
-    Parameters: A dict (dict)
+    Parameters: 
+        dict: A dict (dict)
     Output: None
-    This works best with non-nested dicts
+
+    *This works best with non-nested dicts
 '''
 def pprint(dict):
     # Get keys of dict
@@ -30,9 +32,11 @@ def pprint(dict):
         print(f"{i}:{(long_feat_len-len(i)+1)*' '}{dict[i]}")
 
 
-'''Reading and formatting dataset
-   Parameters: data file (string)
-   Output: Data (dict)
+''' Reading and formatting dataset
+    Parameters: 
+        file: data file (string)
+    Output: 
+        full_dataset_dict: The processed data (dict)
 '''
 def import_data(file):
     # Read full csv dataset file
@@ -63,8 +67,11 @@ def import_data(file):
 
 
 ''' Evaluation of mean and variance of a feature on full dataset
-    Parameters: data (dict), feature (string)
-    Output: mean, variance (dict)
+    Parameters: 
+        data: The imported data (dict)
+        feature: The feature who's mean and variance will be calculated (string)
+    Output: 
+        dict conatining mean and variance (dict)
 '''
 def mean_variance(data, feature):
     # initialize list containing values in the specified feature
@@ -85,8 +92,11 @@ def mean_variance(data, feature):
 
 
 ''' Get iqr of a feature
-    Parameters: data (dict), feature (string)
-    Output: The IQR for the provided data (float)
+    Parameters: 
+        data: the imported data (dict)
+        feature: The feature who's IQR needs to be calculated (string)
+    Output: 
+        iqr: The IQR for the provided data (float)
 '''
 def iqr(data, feature):
     values = []
@@ -103,8 +113,12 @@ def iqr(data, feature):
 
 
 ''' Get minimum/maximum box plot whisker value
-    Parameters: data (dict), feature (string), type (string) (either min, max, or both)
-    Output: Either the value of the smallest whisker, largest whisker, or both (dict)
+    Parameters: 
+        data: The imported data (dict)
+        feature: Feature who's whiskers need to be found (string)
+        type: Specifies if the min whisker, max whisker, or both whiskers should be calculated (string)
+    Output: 
+        Either the value of the smallest whisker, largest whisker, or both (dict)
 '''
 def whisker_values(data, feature, type="both"):
     values = []
@@ -132,8 +146,12 @@ def whisker_values(data, feature, type="both"):
 
 
 ''' Get outliers based on IQR
-    Parameters: data (dict), feature (string)
-    Output: outliers and their index (dict)
+    Parameters: 
+        data: The imported data (dict)
+        feature: The feature who's outliers need to be calculated (string)
+    Output: 
+        outliers: A dict of outliers. The key corresponds to the attribute/sample 
+            number and the value is the value of the outlier (dict)
 '''
 def iqr_outliers(data, feature):
     values = []
@@ -160,6 +178,10 @@ def iqr_outliers(data, feature):
 
 
 ''' Normalizes data such that it falls between 0 and 1
+    Parameters: 
+        data: All the imported data (dict)
+        feature: The feature to normalize (string)
+    Output: None
 '''
 def normalization(data, feature):
     normalized = []
@@ -174,6 +196,10 @@ def normalization(data, feature):
 
 
 ''' Prints information about the specified feature
+    Parameters: 
+        data: All the imported data (dict)
+        feature: The feature who's info you want to print (string)
+    Output: None
 '''
 def print_info(data, feature):
     # Print attributes of the box plot
@@ -207,15 +233,26 @@ def print_info(data, feature):
 
 
 ''' Create box and whiskers plot for the designated feature
+    Parameters:
+        data: The data associated with the required feature (list)
+        feature: The name of the feature (string)
+    Output: None
 '''
 def plot_box(data, feature):
     fig1, ax1 = plt.subplots()
     ax1.set_title(f'{feature} box plot')
-    ax1.boxplot(data[feature])
+    ax1.boxplot(data)
     plt.show()
 
 
 ''' Create scatter plot for 2 features (transformed or not)
+    Parameters:
+        data1: The data for the first feature (list)
+        data2: The data for the second feature (list)
+        feature1: The name of the first feature (string)
+        feature2: The name of the second feature (string)
+    Output: None
+
 '''
 def plot_scatter(data1, data2, feature1, feature2):
     fig1, ax1 = plt.subplots()
@@ -244,7 +281,7 @@ if __name__ == "__main__":
     # pprint(mean_var_dict)
 
     # Plot stuff
-    plot_box(data, features[2])
+    plot_box(data[features[2]], features[2])
     plot_scatter([number ** 1.4 for number in normalization(data, features[2])], normalization(data, features[5]), features[2], features[5])
 
     print_info(data, features[2])
