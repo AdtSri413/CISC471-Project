a3a87b4e7948f2397c7895812802ce368a7f322c
AdtSri413 <aditi.srinivasan@gmail.com>
2022-10-12 18:42:54 -0400

    Added some plotting and normalization functions

--- data_exploration.py
+++ data_exploration.py
@@ -5,6 +5,30 @@ from statistics import mean, median
 from xml.dom import minicompat
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+
+''' pretty printer for dicts
+    Parameters: A dict (dict)
+    Output: None
+    This works best with non-nested dicts
+'''
+def pprint(dict):
+    # Get keys of dict
+    features = list(dict.keys())
+
+    # define variables for length of the longest feature name
+    long_feat_len = 0
+
+    # Find the length of the longest feature name
+    for i in features:
+        if len(i)>long_feat_len:
+            long_feat_len = len(i)
+
+    # print dict
+    for i in features:
+        print(f"{i}:{(long_feat_len-len(i)+1)*' '}{dict[i]}")
+
 
 '''Reading and formatting dataset
    Parameters: data file (string)
@@ -130,31 +154,74 @@ def iqr_outliers(data, feature):
     # for each attribute, if the attribute falls outside the range of min_whisker to max_whisker, add it to the outlier dict
     for i in range(0, len(values)):
         if values[i] < min_whisker or values[i] > max_whisker:
-            outliers[f"attribute {i}"] = values[i]
+            outliers[f"{i}"] = values[i]
 
     return outliers
 
 
-''' pretty printer for dicts
-    Parameters: A dict (dict)
-    Output: None
-    This works best with non-nested dicts
+''' Normalizes data such that it falls between 0 and 1
 '''
-def pprint(dict):
-    # Get keys of dict
-    features = list(dict.keys())
+def normalization(data, feature):
+    normalized = []
+    minimum = min(data[feature])
+    maximum = max(data[feature])
 
+    for i in data[feature]:
+        temp = (i - minimum) / (maximum - minimum)
+        normalized.append(temp)
+
+    return normalized
+
+
+''' Prints information about the specified feature
+'''
+def print_info(data, feature):
+    # Print attributes of the box plot
+    print(f"\n------------{feature.upper()}------------\n")
+    
+    mean_var = mean_variance(data, feature)
+
+    print(f"Min val     = {min(data[feature])}")
+    print(f"Max val     = {max(data[feature])}")
+    print(f"IQR         = {iqr(data, feature)}")
+    print(f"Median      = {median(data[feature])}")
+    print(f"Mean        = {mean_var['mean']}")
+    print(f"Variance    = {mean_var['variance']}")
+    print(f"Min whisker = {whisker_values(data, feature, 'min')['min']}")
+    print(f"Max whisker = {whisker_values(data, feature, 'max')['max']}")
+    print("Outliers: ")
+    outliers = iqr_outliers(data, feature)
+    
     # define variables for length of the longest feature name
     long_feat_len = 0
 
     # Find the length of the longest feature name
-    for i in features:
+    for i in outliers:
         if len(i)>long_feat_len:
             long_feat_len = len(i)
 
-    # print dict
-    for i in features:
-        print(f"{i}:{(long_feat_len-len(i)+1)*' '}{dict[i]}")
+    for i in outliers:
+        print(f"\tAttribute # {i}:{(long_feat_len-len(i)+1)*' '}{outliers[i]}")
+
+    print(f"\n------------{len(feature)*'-'}------------\n")
+
+
+''' Create box and whiskers plot for the designated feature
+'''
+def plot_box(data, feature):
+    fig1, ax1 = plt.subplots()
+    ax1.set_title(f'{feature} box plot')
+    ax1.boxplot(data[feature])
+    plt.show()
+
+
+''' Create scatter plot for 2 features (transformed or not)
+'''
+def plot_scatter(data1, data2, feature1, feature2):
+    fig1, ax1 = plt.subplots()
+    ax1.set_title(f"Plot {feature1} vs. {feature2}")
+    ax1.scatter(data1, data2)
+    plt.show()
 
 
 ''' Main function
@@ -163,34 +230,21 @@ if __name__ == "__main__":
     # import data
     data = import_data("breast-cancer.csv")
 
-    # initialize dictionary to store mean and variance
-    mean_var_dict = {}
-
     # Get list of features
     features = list(data.keys())
+
+    # initialize dictionary to store mean and variance
+    mean_var_dict = {}
 
     # calculate mean and variance for each numerical feature
     for i in features[2:]:
         mean_var_dict[i] = mean_variance(data, i)    
 
     # Print mean and variance of each numerical feature
-    pprint(mean_var_dict)
+    # pprint(mean_var_dict)
 
+    # Plot stuff
+    plot_box(data, features[2])
+    plot_scatter([number ** 1.4 for number in normalization(data, features[2])], normalization(data, features[5]), features[2], features[5])
 
-    # Ex. Create box plot for feature #2: radius_mean
-    fig1, ax1 = plt.subplots()
-    ax1.set_title(f'{features[2]} box plot')
-    ax1.boxplot(data[features[2]])
-    d = plt.boxplot(data[features[2]])
-
-    # Print attributes of the box plot
-    print(f"Min val     = {min(data[features[2]])}")
-    print(f"Max val     = {max(data[features[2]])}")
-    print(f"IQR         = {iqr(data, features[2])}")
-    print(f"Median      = {median(data[features[2]])}")
-    print(f"Min whisker = {whisker_values(data, features[2], 'min')['min']}")
-    print(f"Max whisker = {whisker_values(data, features[2], 'max')['max']}")
-    print("Outliers: ")
-    pprint(iqr_outliers(data, features[2]))
-
-    plt.show()
+    print_info(data, features[2])
